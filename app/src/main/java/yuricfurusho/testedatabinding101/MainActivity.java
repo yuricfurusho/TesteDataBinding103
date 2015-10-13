2f79839179e864c156a299d6a3f4e193ba4834c5
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-10-13 15:35:30 -0300

    exemplo simples

--- app/src/main/java/yuricfurusho/testedatabinding101/MainActivity.java
+++ app/src/main/java/yuricfurusho/testedatabinding101/MainActivity.java
@@ -1,13 +1,20 @@
 package yuricfurusho.testedatabinding101;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import yuricfurusho.testedatabinding101.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        User user = new User("Yuri", "Furusho");
+        binding.setUser(user);
     }
 }
---
+++ app/src/main/java/yuricfurusho/testedatabinding101/User.java
@@ -0,0 +1,14 @@
+package yuricfurusho.testedatabinding101;
+
+/**
+ * Created by live on 13/10/15.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
