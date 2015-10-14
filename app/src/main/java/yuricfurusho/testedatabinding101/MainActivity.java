48468a33c78458198e01f9f3e0acfdab48510708
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-10-14 15:37:10 -0300

    ajustes

--- app/src/main/java/yuricfurusho/testedatabinding101/MainActivity.java
+++ app/src/main/java/yuricfurusho/testedatabinding101/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         User user = new User("Yuri", "Furusho");
