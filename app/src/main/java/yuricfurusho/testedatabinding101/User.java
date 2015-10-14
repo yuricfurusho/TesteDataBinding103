3413356ddcd1b3fd8e0b34b7c1a20943f0adc439
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-10-14 15:34:39 -0300

    ajustes

--- app/src/main/java/yuricfurusho/testedatabinding101/User.java
+++ app/src/main/java/yuricfurusho/testedatabinding101/User.java
@@ -4,8 +4,8 @@ package yuricfurusho.testedatabinding101;
  * Created by live on 13/10/15.
  */
 public class User {
-    public final String firstName;
-    public final String lastName;
+    public String firstName;
+    public String lastName;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
